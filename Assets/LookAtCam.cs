54041bc998c9222e6ffa47e8144b9d3bc8dca1c1
gto <goyafuso.voided@gmail.com>
2020-05-11 11:10:37 -0400

    inital push

---
+++ Assets/LookAtCam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+		//transform.LookAt(Camera.main.transform.position, -Vector3.up);
+		//transform.rotation.Set(0,0,transform.rotation.z,0);
+		transform.rotation=Camera.main.transform.rotation;
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+	public float mvspd = 5;
+
+	Vector3 forward = new Vector3(1,0,0);
+	Vector3 back = new Vector3(-1,0,0);
+	Vector3 up = new Vector3(0,0,1);
+	Vector3 down = new Vector3(0,0,-1);
+    void Start() {
+        
+    }
+
+
+    void Update() {
+        if(Input.GetKey(KeyCode.RightArrow)) {
+			gameObject.transform.Translate(forward*Time.deltaTime*mvspd,Space.World);
+		}
+        if(Input.GetKey(KeyCode.LeftArrow)) {
+			gameObject.transform.Translate(back*Time.deltaTime*mvspd,Space.World);
+		}
+        if(Input.GetKey(KeyCode.UpArrow)) {
+			gameObject.transform.Translate(up*Time.deltaTime*mvspd,Space.World);
+		}
+        if(Input.GetKey(KeyCode.DownArrow)) {
+			gameObject.transform.Translate(down*Time.deltaTime*mvspd,Space.World);
+		}
+    }
+}
