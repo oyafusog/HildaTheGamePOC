b8bc99f6779c9867455ce7453382efc3d923e165
gto <goyafuso.voided@gmail.com>
2020-06-21 05:10:48 -0400

    Added blender file

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,16 +7,31 @@ public class PlayerController : MonoBehaviour {
 
 	public float mvspd = 5;
 
-	Vector3 forward = new Vector3(1,0,0);
-	Vector3 back = new Vector3(-1,0,0);
-	Vector3 up = new Vector3(0,0,1);
-	Vector3 down = new Vector3(0,0,-1);
+	Vector3 forward = new Vector3(-1,0,0);
+	Vector3 back = new Vector3(1,0,0);
+	Vector3 up = new Vector3(0,0,-1);
+	Vector3 down = new Vector3(0,0,1);
+
     void Start() {
         
     }
 
 
     void Update() {
+
+		if(Input.GetKeyDown(KeyCode.Q)) {
+			//Camera.main.transform.Translate( Camera.main.transform.forward *Time.deltaTime,Space.World);
+			Camera.main.orthographicSize *= .9f;
+		}
+		if(Input.GetKeyDown(KeyCode.W)) {
+			//Camera.main.transform.Translate( -Camera.main.transform.forward *Time.deltaTime,Space.World);
+			Camera.main.orthographicSize *= 1.1f;
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space)) {
+
+		}
+
         if(Input.GetKey(KeyCode.RightArrow)) {
 			gameObject.transform.Translate(forward*Time.deltaTime*mvspd,Space.World);
 		}
---
+++ Assets/Scripts/SmoothCam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCam : MonoBehaviour {
+
+	public GameObject follow;
+	public float lerptime;
+
+    void Start() {
+        
+    }
+
+
+    void Update() {
+        Vector3 targetpos = Vector3.Lerp(follow.transform.position,transform.position,lerptime);
+		transform.position = targetpos;
+    }
+}
