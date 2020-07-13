d3999dddbccc6c217d11dc1dbb6405bc5581da60
gto <goyafuso.voided@gmail.com>
2020-07-13 00:17:01 -0400

    Basic waterfall

---
+++ Assets/Scripts/ResetObstacle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Script to put player back to specified position
+/// 
+/// </summary>
+public class ResetObstacle : MonoBehaviour {
+    
+	public Transform reset_position; 
+
+	void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+	void OnTriggerEnter(Collider other ) {
+		if(other.CompareTag("Player")) {
+			other.gameObject.transform.position = reset_position.position;
+		}
+	}
+
+}
---
+++ Assets/Scripts/Scroller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroller : MonoBehaviour {
+
+	public float _X;
+	public float _Y;
+	float OffsetX; 
+	float OffsetY; 
+    void Update() {
+        OffsetX += Time.deltaTime * _X;
+        OffsetY += Time.deltaTime * _Y;
+		GetComponent<Renderer>().material.mainTextureOffset = new Vector2(OffsetX ,OffsetY);
+    }
+}
