ad90fbba9dba158200efbbe3ea362f86ddc63c03
gto <goyafuso.voided@gmail.com>
2020-07-12 22:00:33 -0400

    Weekend commit

--- Assets/Scripts/LookAtCam.cs
+++ Assets/Scripts/LookAtCam.cs
@@ -9,11 +9,12 @@ public class LookAtCam : MonoBehaviour
     {
         
     }
-
     // Update is called once per frame
     void Update() {
-		//transform.LookAt(Camera.main.transform.position, -Vector3.up);
-		//transform.rotation.Set(0,0,transform.rotation.z,0);
+		/*transform.LookAt(Camera.main.transform.position, -Vector3.up);
+		transform.rotation.Set(0,0,transform.rotation.z,0);*/
 		transform.rotation=Camera.main.transform.rotation;
 	}
+
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-
 	public float mvspd = 5;
 
+	//adjusted movements
 	Vector3 forward = new Vector3(-1,0,0);
 	Vector3 back = new Vector3(1,0,0);
 	Vector3 up = new Vector3(0,0,-1);
 	Vector3 down = new Vector3(0,0,1);
 
+	//sprite orientation
+	bool mvrght = true;
+	public GameObject sprite;
+
     void Start() {
-        
     }
 
 
@@ -34,9 +37,11 @@ public class PlayerController : MonoBehaviour {
 
         if(Input.GetKey(KeyCode.RightArrow)) {
 			gameObject.transform.Translate(forward*Time.deltaTime*mvspd,Space.World);
+			mvrght = true;
 		}
         if(Input.GetKey(KeyCode.LeftArrow)) {
 			gameObject.transform.Translate(back*Time.deltaTime*mvspd,Space.World);
+			mvrght = false;
 		}
         if(Input.GetKey(KeyCode.UpArrow)) {
 			gameObject.transform.Translate(up*Time.deltaTime*mvspd,Space.World);
@@ -44,5 +49,30 @@ public class PlayerController : MonoBehaviour {
         if(Input.GetKey(KeyCode.DownArrow)) {
 			gameObject.transform.Translate(down*Time.deltaTime*mvspd,Space.World);
 		}
+
+		SpriteDirection();
+		SpriteLookAtCamera();
     }
+
+	void SpriteLookAtCamera() {
+		Vector3 correction = Camera.main.transform.eulerAngles;
+		correction.y = sprite.transform.eulerAngles.y;
+		if(!mvrght) {
+			correction.x = correction.x*-1;
+		}
+		sprite.transform.eulerAngles = correction;
+	}
+
+	void SpriteDirection() {
+		Debug.Log(mvrght);
+		if(mvrght) {
+			Vector3 orientation = sprite.transform.localEulerAngles;
+			orientation.y = 180f;
+			sprite.transform.eulerAngles = orientation;
+		} else {
+			Vector3 orientation = sprite.transform.localEulerAngles;
+			orientation.y = 0;
+			sprite.transform.eulerAngles = orientation;
+		}
+	}
 }
