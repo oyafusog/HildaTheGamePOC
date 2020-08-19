53d04b9809fc2b3c26a7a3d511f681e9feaeabd2
NuMellow <cmmukabe@gmail.com>
2020-08-18 20:47:23 -0400

    add basic woff script and object Pooling script

---
+++ Assets/Scripts/WoffController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoffController : MonoBehaviour
+{
+    [Header("Set in the Inspector")]
+    public int flySpeed = 5;
+
+    Vector3 forward = new Vector3(-1, 0, 0);
+    Vector3 startPos = new Vector3(72, 0.11f, -5.23f);
+    Vector3 endPos = new Vector3(38, 0.11f, -5.23f);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gameObject.transform.position = startPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Once the trigger point has been hit
+        //Check if the woff position is off screen to the left of camera and setactive is false. If so set it to true
+
+        gameObject.transform.Translate(forward* Time.deltaTime * flySpeed, Space.World);
+
+        //Check if woff position is off camera to the right of camera and setactive is true. If so set false and take position back to left
+        if(gameObject.transform.position.x < endPos.x)
+        {
+            gameObject.transform.position = new Vector3(startPos.x, startPos.y, gameObject.transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/WoffHerdCreator.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoffHerdCreator : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject woff = PoolObject.SharedInstance.GetPooledObject();
+        //Vector3 woffStart = new Vector3(72, 0.11f, Random.Range(-6f, 0f));
+
+        //if (woff != null)
+        //{
+        //    woff.transform.position = woffStart; //Make woff positioni random within some space
+
+        //    woff.SetActive(true);
+        //}
+
+        int poolNum = PoolObject.SharedInstance.GetPoolNum();
+
+        for(int i = 0; i < poolNum; i++)
+        {
+            GameObject woff = PoolObject.SharedInstance.GetPooledObject();
+            Vector3 woffStart = new Vector3(72, 0.11f, Random.Range(-6f, 0f));
+
+            if(woff != null)
+            {
+                woff.transform.position = woffStart;
+                woff.SetActive(true);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject woff = PoolObject.SharedInstance.GetPooledObject();
+        Vector3 woffStart = new Vector3(72, Random.Range(0.11f, 3f), Random.Range(-6f, 0f));
+
+        if (woff != null)
+        {
+            woff.transform.position = woffStart; //Make woff positioni random within some space
+
+            woff.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("player"))
+        {
+            CreateWoffHerd();
+        }
+    }
+
+    void CreateWoffHerd()
+    {
+        GameObject woff = PoolObject.SharedInstance.GetPooledObject();
+        Vector3 woffStart = new Vector3(72, 0.11f, Random.Range(-6f, 0f));
+
+        if (woff != null)
+        {
+            woff.transform.position = woffStart; //Make woff positioni random within some space
+
+            woff.SetActive(true);
+        }
+    }
+}
